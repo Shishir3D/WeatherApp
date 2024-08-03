e282291fae6bfa4a18fb234df089a9f3c2b1c267
shishir3d <shishirpoudel2061@gmail.com>
2024-08-03 08:29:16 +0545

    update: center the ui on web

--- lib/weather_app.dart
+++ lib/weather_app.dart
@@ -76,193 +76,201 @@ class _WeatherScreenState extends State<WeatherScreen> {
           final currentHumidity = data['list'][0]['main']['humidity'];
           final currentWindSpeed = data['list'][0]['wind']['speed'];
 
-          return Padding(
-            padding: const EdgeInsets.all(16.0),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                //main Card
-                SizedBox(
-                  width: double.infinity,
-                  child: Card(
-                    elevation: 10,
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(20),
-                    ),
-                    child: ClipRRect(
-                      borderRadius: const BorderRadius.all(Radius.circular(16)),
-                      child: BackdropFilter(
-                        filter: ImageFilter.blur(sigmaX: 10, sigmaY: 10),
-                        child: Padding(
-                          padding: const EdgeInsets.all(16.0),
-                          child: Column(
-                            children: [
-                              Text(
-                                currentTemperature,
-                                style: const TextStyle(
-                                  fontSize: 32,
-                                  fontWeight: FontWeight.bold,
-                                ),
+          return Center(
+            child: SizedBox(
+              width: 432,
+              child: Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    //main Card
+                    SizedBox(
+                      width: double.infinity,
+                      child: Card(
+                        elevation: 10,
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(20),
+                        ),
+                        child: ClipRRect(
+                          borderRadius:
+                              const BorderRadius.all(Radius.circular(16)),
+                          child: BackdropFilter(
+                            filter: ImageFilter.blur(sigmaX: 10, sigmaY: 10),
+                            child: Padding(
+                              padding: const EdgeInsets.all(16.0),
+                              child: Column(
+                                children: [
+                                  Text(
+                                    currentTemperature,
+                                    style: const TextStyle(
+                                      fontSize: 32,
+                                      fontWeight: FontWeight.bold,
+                                    ),
+                                  ),
+                                  const SizedBox(height: 10),
+                                  Icon(
+                                    currentSky == "Sunny"
+                                        ? Icons.sunny
+                                        : Icons.cloud,
+                                    size: 64,
+                                  ),
+                                  const SizedBox(height: 10),
+                                  Text(
+                                    currentSky,
+                                    style: const TextStyle(
+                                      fontSize: 22,
+                                    ),
+                                  ),
+                                ],
                               ),
-                              const SizedBox(height: 10),
-                              Icon(
-                                currentSky == "Sunny"
-                                    ? Icons.sunny
-                                    : Icons.cloud,
-                                size: 64,
-                              ),
-                              const SizedBox(height: 10),
-                              Text(
-                                currentSky,
-                                style: const TextStyle(
-                                  fontSize: 22,
-                                ),
-                              ),
-                            ],
+                            ),
                           ),
                         ),
                       ),
                     ),
-                  ),
-                ),
 
-                const SizedBox(height: 20),
+                    const SizedBox(height: 20),
 
-                //weather forcast Cards
-                const Text(
-                  "Today's Forecast",
-                  style: TextStyle(
-                    fontSize: 24,
-                    fontWeight: FontWeight.bold,
-                  ),
-                ),
-
-                const SizedBox(
-                  height: 12,
-                ),
-
-                SizedBox(
-                  height: 140,
-                  child: ListView.builder(
-                    scrollDirection: Axis.horizontal,
-                    itemCount: 25,
-                    itemBuilder: (context, index) {
-                      final hourlyForecast = data['list'][index + 1];
-                      final hourlySky = hourlyForecast['weather'][0]['main'];
-                      final time = DateTime.parse(hourlyForecast['dt_txt']);
-
-                      return HourlyForcastItem(
-                        time: DateFormat.j().format(time).toString(),
-                        icon: hourlySky == 'Rain'
-                            ? Icons.thunderstorm
-                            : hourlySky == 'Clouds'
-                                ? Icons.cloud
-                                : Icons.sunny,
-                        temperature:
-                            "${((data['list'][index + 1]['main']['temp']) - 273.15).floor()} °C",
-                      );
-                    },
-                  ),
-                ),
-
-                const SizedBox(
-                  height: 10,
-                ),
-
-                const Text(
-                  "Additional Information",
-                  style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
-                ),
-
-                const SizedBox(
-                  height: 20,
-                ),
-
-                //additional information
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceAround,
-                  children: [
-                    AdditionalInfoItems(
-                      icon: Icons.water_drop,
-                      info: "Humidity",
-                      value: currentHumidity.toString(),
+                    //weather forcast Cards
+                    const Text(
+                      "Today's Forecast",
+                      style: TextStyle(
+                        fontSize: 24,
+                        fontWeight: FontWeight.bold,
+                      ),
                     ),
-                    AdditionalInfoItems(
-                      icon: Icons.air,
-                      info: "Wind Speed",
-                      value: currentWindSpeed.toString(),
+
+                    const SizedBox(
+                      height: 12,
                     ),
-                    AdditionalInfoItems(
-                      icon: Icons.beach_access,
-                      info: "Pressure",
-                      value: currentPressure.toString(),
+
+                    SizedBox(
+                      height: 140,
+                      child: ListView.builder(
+                        scrollDirection: Axis.horizontal,
+                        itemCount: 25,
+                        itemBuilder: (context, index) {
+                          final hourlyForecast = data['list'][index + 1];
+                          final hourlySky =
+                              hourlyForecast['weather'][0]['main'];
+                          final time = DateTime.parse(hourlyForecast['dt_txt']);
+
+                          return HourlyForcastItem(
+                            time: DateFormat.j().format(time).toString(),
+                            icon: hourlySky == 'Rain'
+                                ? Icons.thunderstorm
+                                : hourlySky == 'Clouds'
+                                    ? Icons.cloud
+                                    : Icons.sunny,
+                            temperature:
+                                "${((data['list'][index + 1]['main']['temp']) - 273.15).floor()} °C",
+                          );
+                        },
+                      ),
                     ),
-                  ],
-                ),
-                const SizedBox(
-                  height: 20,
-                ),
-                Center(
-                  child: SizedBox(
-                    child: DropdownButton<String>(
-                      value: cityName,
-                      icon: const Icon(Icons.keyboard_double_arrow_down),
-                      onChanged: (String? newValue) {
-                        setState(() {
-                          cityName = newValue;
-                        });
-                      },
-                      items: const [
-                        DropdownMenuItem(
-                          value: "Kathmandu",
-                          child: Text(
-                            "Kathmandu",
-                            style: TextStyle(
-                              fontSize: 22,
-                            ),
-                          ),
+
+                    const SizedBox(
+                      height: 10,
+                    ),
+
+                    const Text(
+                      "Additional Information",
+                      style:
+                          TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+                    ),
+
+                    const SizedBox(
+                      height: 20,
+                    ),
+
+                    //additional information
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceAround,
+                      children: [
+                        AdditionalInfoItems(
+                          icon: Icons.water_drop,
+                          info: "Humidity",
+                          value: currentHumidity.toString(),
                         ),
-                        DropdownMenuItem(
-                          value: "Tokha",
-                          child: Text(
-                            "Tokha",
-                            style: TextStyle(
-                              fontSize: 22,
-                            ),
-                          ),
+                        AdditionalInfoItems(
+                          icon: Icons.air,
+                          info: "Wind Speed",
+                          value: currentWindSpeed.toString(),
                         ),
-                        DropdownMenuItem(
-                          value: "Pokhara",
-                          child: Text(
-                            "Pokhara",
-                            style: TextStyle(
-                              fontSize: 22,
-                            ),
-                          ),
-                        ),
-                        DropdownMenuItem(
-                          value: "Biratnagar",
-                          child: Text(
-                            "Biratnagar",
-                            style: TextStyle(
-                              fontSize: 22,
-                            ),
-                          ),
-                        ),
-                        DropdownMenuItem(
-                          value: "Bhairahawa",
-                          child: Text(
-                            "Bhairahawa",
-                            style: TextStyle(
-                              fontSize: 22,
-                            ),
-                          ),
+                        AdditionalInfoItems(
+                          icon: Icons.beach_access,
+                          info: "Pressure",
+                          value: currentPressure.toString(),
                         ),
                       ],
                     ),
-                  ),
+                    const SizedBox(
+                      height: 20,
+                    ),
+                    Center(
+                      child: SizedBox(
+                        child: DropdownButton<String>(
+                          value: cityName,
+                          icon: const Icon(Icons.keyboard_double_arrow_down),
+                          onChanged: (String? newValue) {
+                            setState(() {
+                              cityName = newValue;
+                            });
+                          },
+                          items: const [
+                            DropdownMenuItem(
+                              value: "Kathmandu",
+                              child: Text(
+                                "Kathmandu",
+                                style: TextStyle(
+                                  fontSize: 22,
+                                ),
+                              ),
+                            ),
+                            DropdownMenuItem(
+                              value: "Tokha",
+                              child: Text(
+                                "Tokha",
+                                style: TextStyle(
+                                  fontSize: 22,
+                                ),
+                              ),
+                            ),
+                            DropdownMenuItem(
+                              value: "Pokhara",
+                              child: Text(
+                                "Pokhara",
+                                style: TextStyle(
+                                  fontSize: 22,
+                                ),
+                              ),
+                            ),
+                            DropdownMenuItem(
+                              value: "Biratnagar",
+                              child: Text(
+                                "Biratnagar",
+                                style: TextStyle(
+                                  fontSize: 22,
+                                ),
+                              ),
+                            ),
+                            DropdownMenuItem(
+                              value: "Bhairahawa",
+                              child: Text(
+                                "Bhairahawa",
+                                style: TextStyle(
+                                  fontSize: 22,
+                                ),
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                  ],
                 ),
-              ],
+              ),
             ),
           );
         },
